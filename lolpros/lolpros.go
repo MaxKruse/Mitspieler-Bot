1df789d293d97a46991fd369e0b310a2f64faa60
Maximilian Kruse <maxkruse1996@gmail.com>
2021-08-16 01:46:50 +0200

    Finish lolpros script

--- lolpros/lolpros.go
+++ lolpros/lolpros.go
@@ -7,10 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"math"
-	"math/rand"
 	"net/http"
 	"sync"
-	"time"
 
 	// import gorm
 	"gorm.io/driver/postgres"
@@ -22,6 +20,7 @@ type LadderEntry struct {
 	Name string `json:"name"`
 }
 
+// Database Specifics
 type Player struct {
 	gorm.Model
 	Name     string    `json:"name"`
@@ -30,8 +29,18 @@ type Player struct {
 
 type Account struct {
 	gorm.Model
-	PlayerId     int64
+	PlayerId     int64  `gorm:",primary_key"`
 	SummonerName string `json:"summoner_name"`
+}
+
+// Riot Specifics
+type RiotPlayer struct {
+	LeaguePlayer `json:"league_player"`
+	Name         string `json:"name"`
+}
+
+type LeaguePlayer struct {
+	Accounts []Account `json:"accounts"`
 }
 
 const (
@@ -49,6 +58,8 @@ var (
 	httpClient = &http.Client{}
 
 	players = flag.Int("players", 50, "Number of players to pull")
+
+	db *gorm.DB
 )
 
 func getLadderUrl(page int) string {
@@ -62,7 +73,7 @@ func getPlayerUrl(player string) string {
 func makeApiCall(url string) ([]byte, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 		return nil, err
 	}
 	req.Header.Add("Accept", "application/json")
@@ -70,30 +81,79 @@ func makeApiCall(url string) ([]byte, error) {
 
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 		return nil, err
 	}
 
 	return bytes, nil
 }
 
-func makeLadderEntry(bytes []byte) (LadderEntry, error) {
-	var entry LadderEntry
+func makeLadderEntries(bytes []byte) ([]LadderEntry, error) {
+	var entry []LadderEntry
 	err := json.Unmarshal(bytes, &entry)
 	return entry, err
 }
 
-func makePlayer(bytes []byte) (Player, error) {
-	var player Player
+func makePlayer(bytes []byte) (RiotPlayer, error) {
+	var player RiotPlayer
 	err := json.Unmarshal(bytes, &player)
 	return player, err
+}
+
+func prettyPrint(str interface{}) {
+	strJson, _ := json.MarshalIndent(str, "", "  ")
+	log.Printf("%s\n", string(strJson))
+}
+
+func populatePage(wg *sync.WaitGroup, page int) {
+	defer wg.Done()
+
+	// Do actual work with the page
+	url := getLadderUrl(page)
+	bytes, err := makeApiCall(url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	entries, err := makeLadderEntries(bytes)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for _, entry := range entries {
+		playerUrl := getPlayerUrl(entry.Name)
+		bytes, err := makeApiCall(playerUrl)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		riotplayer, err := makePlayer(bytes)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		var player Player
+		player.Accounts = riotplayer.LeaguePlayer.Accounts
+		player.Name = riotplayer.Name
+
+		var local Player
+		db.First(&local, player)
+		// Only create entry if player is not in db
+		if local.ID < 1 {
+			db.Model(&player).Save(&player)
+		}
+	}
 }
 
 func main() {
@@ -102,15 +162,16 @@ func main() {
 	flag.Parse()
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Berlin", DB_HOST, DB_USER, DB_PASS, "lolpros", DB_PORT)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	err := error(nil)
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 		return
 	}
 
 	// create the tables
-	db.AutoMigrate(Player{})
+	db.AutoMigrate(Player{}, Account{})
 
 	// make a go routine for each page index until 5
 	count := int(math.Ceil(float64(*players) / float64(50)))
@@ -119,15 +180,21 @@ func main() {
 	wg.Add(count)
 
 	for i := 1; i <= int(count); i++ {
-		go func(page int, wg *sync.WaitGroup) {
-			defer wg.Done()
+		go populatePage(&wg, i)
 
-			// Sleep between 0 and 5 seconds
-			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
-			log.Println("Hello World from", page)
-		}(i, &wg)
+		// Sleep between 0 and 5 seconds
+
+		// Example code
+		// p := &Player{
+		// 	Name: "Agurin",
+		// 	Accounts: []Account{
+		// 		{SummonerName: "Agurin"},
+		// 		{SummonerName: "Charlie Heaton"},
+		// 	},
+		// }
+		// db.Model(Player{}).FirstOrCreate(p)
+
 	}
 
 	wg.Wait()
-	log.Println("I did it mom!")
 }
